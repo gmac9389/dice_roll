772845502cd6731ea7ad9ecb994c51bee25c349c
gmac9389 <32885943+gmac9389@users.noreply.github.com>
2017-10-18 01:14:19 +0100

    Add files via upload

---
+++ dice_roll_game.py
@@ -0,0 +1,20 @@
+import random
+random.random()
+
+def dice_roll():
+    roll = 'You rolled a: ', random.randint(1,6)
+    return roll
+
+
+user_input = ''
+
+while user_input != 'Quit':
+    user_input = input('Hi, this is the random dice roll game.\nTo roll the dice type ROLL.\nTo quit the game type QUIT.\nOk let\'s start: ')
+    if user_input.lower() == 'roll':
+        print(dice_roll())
+    elif user_input == 'quit':
+        print('Thank you for playing')
+        break
+    else:
+        print('Invalid input')
+    

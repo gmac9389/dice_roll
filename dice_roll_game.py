3d3c56b655fec667ee66a6a255912380885dbcb0
gmac9389 <32885943+gmac9389@users.noreply.github.com>
2017-10-18 01:15:35 +0100

    Update dice_roll_game.py

--- dice_roll_game.py
+++ dice_roll_game.py
@@ -12,7 +12,7 @@ while user_input != 'Quit':
     user_input = input('Hi, this is the random dice roll game.\nTo roll the dice type ROLL.\nTo quit the game type QUIT.\nOk let\'s start: ')
     if user_input.lower() == 'roll':
         print(dice_roll())
-    elif user_input == 'quit':
+    elif user_input.lower() == 'quit':
         print('Thank you for playing')
         break
     else:
